e0172810f16474e1636634136ea2ea6831db4aff
kemgarro <k.garro.3@estudiantec.cr>
2026-04-26 22:16:15 -0600

    feat: mejorar interpretacion de lenguaje natural

--- orientador_bnf.pl
+++ orientador_bnf.pl
@@ -44,6 +44,14 @@ sintagma_verbal --> enlace, sintagma_verbal.
 
 complemento --> sintagma_nominal.
 complemento --> preposicion, sustantivo.
+complemento --> lista_elementos.
+
+lista_elementos --> elemento.
+lista_elementos --> elemento, [y], lista_elementos.
+lista_elementos --> elemento, [mas], lista_elementos.
+
+elemento --> sustantivo.
+elemento --> adjetivo.
 
 sustantivo --> [programacion].
 sustantivo --> [tecnologia].
@@ -51,10 +59,15 @@ sustantivo --> [matematicas].
 sustantivo --> [arte].
 sustantivo --> [fisica].
 sustantivo --> [biologia].
+sustantivo --> [quimica].
 sustantivo --> [personas].
 sustantivo --> [computadoras].
 sustantivo --> [ideas].
 sustantivo --> [naturaleza].
+sustantivo --> [analisis].
+sustantivo --> [logica].
+sustantivo --> [espanol].
+sustantivo --> [ingles].
 
 pronombre --> [yo].
 pronombre --> [me].
@@ -63,22 +76,33 @@ determinante --> [el].
 determinante --> [la].
 determinante --> [los].
 determinante --> [las].
+determinante --> [un].
+determinante --> [una].
 
 adjetivo --> [creativo].
 adjetivo --> [artistico].
 adjetivo --> [analitico].
+adjetivo --> [analitica].
 adjetivo --> [social].
+adjetivo --> [logico].
+adjetivo --> [logica].
 
 verbo --> [gusta].
 verbo --> [gustan].
 verbo --> [encanta].
+verbo --> [encantan].
 verbo --> [apasiona].
+verbo --> [apasionan].
 verbo --> [interesa].
+verbo --> [interesan].
 verbo --> [prefiero].
+verbo --> [cuesta].
+verbo --> [aburre].
 
 preposicion --> [con].
 preposicion --> [en].
 preposicion --> [para].
+preposicion --> [de].
 
 nexo --> [y].
 nexo --> [mas].
@@ -86,6 +110,7 @@ nexo --> [pero].
 
 enlace --> [pero, no].
 enlace --> [aunque, no].
+enlace --> [sin, embargo].
 
 % ============================================================
 % MARCADORES DCG
@@ -93,28 +118,58 @@ enlace --> [aunque, no].
 
 marcador_positivo --> [me, gusta], !.
 marcador_positivo --> [me, gusta, mucho], !.
+marcador_positivo --> [me, gusta, bien], !.
 marcador_positivo --> [me, encanta], !.
+marcador_positivo --> [me, encanta, mucho], !.
 marcador_positivo --> [me, apasiona], !.
+marcador_positivo --> [me, apasiona, mucho], !.
 marcador_positivo --> [me, interesa], !.
+marcador_positivo --> [me, interesan], !.
+marcador_positivo --> [me, llama, la, atencion], !.
 marcador_positivo --> [soy], !.
 marcador_positivo --> [soy, bueno], !.
 marcador_positivo --> [soy, buena], !.
+marcador_positivo --> [soy, bueno, en], !.
+marcador_positivo --> [soy, buena, en], !.
+marcador_positivo --> [soy, analitico], !.
+marcador_positivo --> [soy, analitica], !.
+marcador_positivo --> [soy, social], !.
+marcador_positivo --> [soy, creativa], !.
+marcador_positivo --> [soy, creativo], !.
 marcador_positivo --> [prefiero], !.
+marcador_positivo --> [prefiero, trabajar], !.
 marcador_positivo --> [disfruto], !.
+marcador_positivo --> [disfruto, de], !.
 marcador_positivo --> [amo], !.
+marcador_positivo --> [adoro], !.
 marcador_positivo --> [tengo, facilidad], !.
+marcador_positivo --> [tengo, facilidad, para], !.
+marcador_positivo --> [me, siento, bien], !.
 
 marcador_negativo --> [no, me, gusta], !.
 marcador_negativo --> [no, me, interesa], !.
+marcador_negativo --> [no, me, interesan], !.
+marcador_negativo --> [no, soy, bueno], !.
+marcador_negativo --> [no, soy, buena], !.
 marcador_negativo --> [me, cuesta], !.
+marcador_negativo --> [me, cuesta, mucho], !.
+marcador_negativo --> [me, cuestan], !.
 marcador_negativo --> [me, aburre], !.
+marcador_negativo --> [me, aburren], !.
 marcador_negativo --> [odio], !.
 marcador_negativo --> [detesto], !.
+marcador_negativo --> [aborrezco], !.
 marcador_negativo --> [evito], !.
 marcador_negativo --> [nunca], !.
+marcador_negativo --> [jamas], !.
 marcador_negativo --> [ni], !.
 marcador_negativo --> [tampoco], !.
 marcador_negativo --> [pero, no], !.
+marcador_negativo --> [aunque, no], !.
+marcador_negativo --> [soy, malo], !.
+marcador_negativo --> [soy, mala], !.
+marcador_negativo --> [soy, malo, en], !.
+marcador_negativo --> [soy, mala, en], !.
 
 % ============================================================
 % INTERFAZ DCG PÚBLICA
@@ -145,11 +200,9 @@ analizar(Tokens, _, Pares) :-
     detectar_marcador(Tokens, NuevaPol, Resto), !,
     analizar(Resto, NuevaPol, Pares).
 analizar([W1, W2 | Resto], Pol, [(K, Pol) | Pares]) :-
-    sinonimo(W1, K1), sinonimo(W2, K2),
-    (   K1 = K2 -> K = K1
-    ;   atom_concat(K1, '_', Aux), atom_concat(Aux, K2, K)
-    ),
-    sinonimo(K, _), !,
+    atom_concat(W1, '_', Tmp),
+    atom_concat(Tmp, W2, Bigrama),
+    sinonimo(Bigrama, K), !,
     analizar(Resto, Pol, Pares).
 analizar([W | Resto], Pol, [(K, Pol) | Pares]) :-
     sinonimo(W, K), !,
@@ -157,20 +210,64 @@ analizar([W | Resto], Pol, [(K, Pol) | Pares]) :-
 analizar([_ | Resto], Pol, Pares) :-
     analizar(Resto, Pol, Pares).
 
+% ============================================================
+% DETECCIÓN DE MARCADORES
+% ============================================================
+
 detectar_marcador([me, gusta | R], positivo, R) :- !.
 detectar_marcador([me, gusta, mucho | R], positivo, R) :- !.
+detectar_marcador([me, gusta, bien | R], positivo, R) :- !.
 detectar_marcador([me, encanta | R], positivo, R) :- !.
+detectar_marcador([me, encanta, mucho | R], positivo, R) :- !.
 detectar_marcador([me, apasiona | R], positivo, R) :- !.
+detectar_marcador([me, apasiona, mucho | R], positivo, R) :- !.
 detectar_marcador([me, interesa | R], positivo, R) :- !.
+detectar_marcador([me, interesan | R], positivo, R) :- !.
+detectar_marcador([me, llama, la, atencion | R], positivo, R) :- !.
 detectar_marcador([soy | R], positivo, R) :- !.
 detectar_marcador([soy, bueno | R], positivo, R) :- !.
 detectar_marcador([soy, buena | R], positivo, R) :- !.
+detectar_marcador([soy, bueno, en | R], positivo, R) :- !.
+detectar_marcador([soy, buena, en | R], positivo, R) :- !.
+detectar_marcador([soy, analitico | R], positivo, R) :- !.
+detectar_marcador([soy, analitica | R], positivo, R) :- !.
+detectar_marcador([soy, social | R], positivo, R) :- !.
+detectar_marcador([soy, creativa | R], positivo, R) :- !.
+detectar_marcador([soy, creativo | R], positivo, R) :- !.
 detectar_marcador([prefiero | R], positivo, R) :- !.
+detectar_marcador([prefiero, trabajar | R], positivo, R) :- !.
+detectar_marcador([disfruto | R], positivo, R) :- !.
+detectar_marcador([disfruto, de | R], positivo, R) :- !.
 detectar_marcador([amo | R], positivo, R) :- !.
+detectar_marcador([adoro | R], positivo, R) :- !.
+detectar_marcador([tengo, facilidad | R], positivo, R) :- !.
+detectar_marcador([tengo, facilidad, para | R], positivo, R) :- !.
+detectar_marcador([me, siento, bien | R], positivo, R) :- !.
+
 detectar_marcador([no, me, gusta | R], negativo, R) :- !.
+detectar_marcador([no, me, interesa | R], negativo, R) :- !.
+detectar_marcador([no, me, interesan | R], negativo, R) :- !.
+detectar_marcador([no, soy, bueno | R], negativo, R) :- !.
+detectar_marcador([no, soy, buena | R], negativo, R) :- !.
 detectar_marcador([me, cuesta | R], negativo, R) :- !.
+detectar_marcador([me, cuesta, mucho | R], negativo, R) :- !.
+detectar_marcador([me, cuestan | R], negativo, R) :- !.
+detectar_marcador([me, aburre | R], negativo, R) :- !.
+detectar_marcador([me, aburren | R], negativo, R) :- !.
 detectar_marcador([odio | R], negativo, R) :- !.
 detectar_marcador([detesto | R], negativo, R) :- !.
+detectar_marcador([aborrezco | R], negativo, R) :- !.
+detectar_marcador([evito | R], negativo, R) :- !.
+detectar_marcador([nunca | R], negativo, R) :- !.
+detectar_marcador([jamas | R], negativo, R) :- !.
+detectar_marcador([ni | R], negativo, R) :- !.
+detectar_marcador([tampoco | R], negativo, R) :- !.
+detectar_marcador([pero, no | R], negativo, R) :- !.
+detectar_marcador([aunque, no | R], negativo, R) :- !.
+detectar_marcador([soy, malo | R], negativo, R) :- !.
+detectar_marcador([soy, mala | R], negativo, R) :- !.
+detectar_marcador([soy, malo, en | R], negativo, R) :- !.
+detectar_marcador([soy, mala, en | R], negativo, R) :- !.
 
 separar_pares([], [], []).
 separar_pares([(K, positivo) | R], [K | Pos], Neg) :- !,
@@ -258,3 +355,7 @@ sinonimo(paciente, paciente).
 sinonimo(comunicativo, comunicativo).
 sinonimo(reflexivo, reflexivo).
 sinonimo(imaginativo, imaginativo).
+sinonimo(espanol, idiomas).
+sinonimo(ingles, idiomas).
+sinonimo(musica, musica).
+sinonimo(matematicas, matematicas).
